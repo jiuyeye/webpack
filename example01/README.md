0aa66c14930608f8dddf03e24b0ab47b1999d24e
lcq0202 <lcq0202@163.com>
2017-02-21 15:29:47 +0800

    webpack

---
+++ example01/README.md
@@ -0,0 +1,4 @@
+### webpack使用
+```javascript
+webpack hello.js hello.bundle.js
+```
--- src/react1.js
+++ src/react1.js
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Hello = React.createClass({
   render: function render() {
-    return <div>Hello {this.props.name}</div>;
+    return <div>Helloddd {this.props.name}</div>;
   }
 });
 
