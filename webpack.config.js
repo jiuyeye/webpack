15af4094665c8f1e27cbef6c466c7c7f50212931
lcq0202 <lcq0202@163.com>
2017-02-21 18:39:46 +0800

    debug

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,12 @@ module.exports = {
 		new htmlWebpackPlugin({
 			filename:"index.html",//文件名
 			template:"index.html",//模板
-			inject:"head",//嵌入位置
+			inject:false,//嵌入位置
 			title:"webpack test demo",//自定义参数变量
-			date:new Date()
+			date:new Date(),
+			minify:{
+				removeComments:true,
+			}
 		})
 	]
 }
